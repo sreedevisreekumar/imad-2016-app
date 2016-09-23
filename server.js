cc849197524dca8f144d0f853f034defd850880b
sreedevi <sreedevivp4u@gmail.com>
2016-09-23 10:24:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -77,6 +77,9 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/:articleName', function (req, res) {
     //articleName=article-one
     //articles[articleName]=={}content object for article one
