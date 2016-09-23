7d8d6899f4044f905707b17be61232ba19066550
sreedevi <sreedevivp4u@gmail.com>
2016-09-23 13:01:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,3 +5,11 @@ element.innerHTML="Hello world";
 alert(element.innerHTML);
 };
 var imgMadi=document.getElementById("madi");
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+1;
+    imgMadi.style.marginLeft=marginLeft  + 'px';
+}
+imgMadi.onclick=function(){
+    var interval=setInterval(moveRight,50);
+}
