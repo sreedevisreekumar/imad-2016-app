1dadbdd09bf3f1de2adcd61ec9df2f91b50bc880
sreedevi <sreedevivp4u@gmail.com>
2016-09-23 11:50:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,6 +73,9 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/favicon.ico', function (req, res) {
+  res.sendFile(path.join(__dirname, '', 'favicon.ico'));
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
