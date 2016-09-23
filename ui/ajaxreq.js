60a35f6658bfcc383d5aceaae0717d09c0b97821
sreedevi <sreedevivp4u@gmail.com>
2016-09-23 20:09:27 +0530

    [imad-console] Updates ui/ajaxreq.js

--- ui/ajaxreq.js
+++ ui/ajaxreq.js
@@ -1,1 +1,12 @@
 
+//counter code
+var btnClick=document.getElementById("btnClick");
+var counter =0;
+btnClick.onclick=function(){
+    //Make a request to the counter endpoint
+    //Capture the response and store in a variable
+    //Render the variable in the correct span
+    counter=counter+1;
+    var count=document.getElementById("count");
+    count.innerHTML =counter.toString();
+};
