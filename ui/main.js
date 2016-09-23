fd694f58f82436c61870b0d16da11015d9147bab
sreedevi <sreedevivp4u@gmail.com>
2016-09-23 12:48:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 window.onload=function(){
 var element=document.getElementById("txtdiv");
-document.getElementById("txtdiv").innerHtml="Hello world";
+element.innerHtml="Hello world";
 alert(element.innerHtml);
 };
 var imgMadi=document.getElementById("madi");
