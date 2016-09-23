ddfa22a6dda5f45433935dc2db229ce97c126365
sreedevi <sreedevivp4u@gmail.com>
2016-09-23 18:25:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,17 +91,18 @@ app.get('/ui/main.js', function (req, res) {
 app.get('/ui/ajaxreq.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'ajaxreq.js'));
 });
+var counter=0;
+app.get('/counter', function (req, res) {
+    counter=counter+1;
+  res.send(counter.toString());
+});
 app.get('/:articleName', function (req, res) {
     //articleName=article-one
     //articles[articleName]=={}content object for article one
     var articleName=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
   });
-var counter=0;
-app.get('/counter', function (req, res) {
-    counter=counter+1;
-  res.send(counter.toString());
-});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
